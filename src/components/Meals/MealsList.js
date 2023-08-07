a226a2188599f13f1e901c2640028cd7802cb60e
Salov Ivan <singlelol21@gmail.com>
2023-08-07 16:54:22 +0300

    fix: edit id in MealItemForm

--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -37,6 +37,7 @@ const MealsList = () => {
   const mealList = DUMMY_MEALS.map((meal) =>
   <MealItem
   key={meal.id}
+  id={meal.id}
   name={meal.name}
   description={meal.description}
   price={meal.price}
