644ab7bdb3863c867152e81b73f1a679477cfe0c
Salov Ivan <singlelol21@gmail.com>
2023-08-10 13:30:06 +0300

    add: add context provider functions

--- src/store/CartContextProvider.js
+++ src/store/CartContextProvider.js
@@ -1,14 +1,44 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReduser = (state, action) => {
+  if( action.type === "ADD_ITEM") {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = 
+    state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  return defaultCartState;
+}
+
 const CardContextProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReduser, defaultCartState);
 
-  const addItemHandler = item => {};
+  const addItemHandler = item => {
+    dispatchCartAction({
+      type: "ADD_ITEM",
+      item: item
+    });
+  };
 
-  const removeItemHandler = id => {};
+  const removeItemHandler = id => {
+    dispatchCartAction({
+      type: "REMOVE_ITEM",
+      id: id
+    });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler
   }
